6c8dd88f41d45574ff475feaeb401a064b25bf7b
popkaloloshka <52071843+popkaloloshka@users.noreply.github.com>
2019-08-10 18:41:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("Запуск бота");
+const fs = require('fs');
+const vkio = require('vk-io');
+const token = "TOKEN";
+const VkBot = require("node-vk-bot-api"),
+bot = new VkBot(token);
+const spam = fs.readFileSync('txt/666.txt', 'utf8');
+console.log("Бот успешно запущен!");
+bot.command("bts", msg =>{
+  msg.reply("who is this?");
+});
+bot.command("/about", msg =>{
+  msg.reply("the description of the bot");
+});
+
+bot.command("/666", msg =>{
+  msg.reply(spam);
+  msg.reply(spam);
+  msg.reply(spam);
+  msg.reply(spam);
+  msg.reply(spam);
+  msg.reply(spam);
+  msg.reply(spam);
+  msg.reply(spam);
+});
+bot.startPolling();
